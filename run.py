cb97319b6f23541f63f603ba11d55dd71d771280
Ozi-cheri Ihemedu <silvernasa@myyahoo.com>
2024-08-18 13:00:59 +0000

    Add prints function and inputs texts

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
 
+print("---Back To School OnLine Shop --- ")
+name = input("Please enter your name:")
+print("Hello "+name)
+print("Welcome To Back To School Online Shop")
+print("*************************************")
+print("Would you like to continue or exist")
+input ("Please enter Yes or No")
