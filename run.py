25425fe3e15983d2ec990602ffb14a35265d6c91
Ozi-cheri Ihemedu <silvernasa@myyahoo.com>
2024-08-18 21:35:00 +0000

    Add list items and modify run.py file

--- run.py
+++ run.py
@@ -1,12 +1,41 @@
 list = []
+
+
+
 print("---Back To School OnLine Shop --- ")
 name = input("Please enter your name:")
 print("Hello "+name)
 print("Welcome To Back To School Online Shop")
-print("*************************************")
+print(''"*************************************")
 
-list = ['pen 0.80€,' 'pencil 0.49€', 'notebook 1.5€,' 'backpack 13.99€,' 'lunchbox 2.5€,' 'calculator 1.30€,' 'ruler 0.99' 'erasers 0.15 ,' 'folders 0.10 ,' 'glue 1€']
+
+print("1. View list")
+print("2. Start shopping")
+print("3. Remove item") 
+print("4. Add item")
+print("5. Quit shopping") 
+
+if not list:
+    print("your cart is empty")
+else:
+    print("list:")
+
+    total_price = 0
+
+choice = input("Enter your choice")
+if choice == '1':
+    item_name = input("Enter item name:")
+
+
+
+
+list = ['pen 0.80€', 'pencil 0.49€', 'notebook 1.5€', 'backpack 13.99€', 'lunchbox 2.5€', 'calculator 1.30€', 'ruler 0.99','erasers 0.15 ', 'folders 0.10 ', 'glue 1€']
+
+def displayList() :
+    print ()
+    print ("LIST")
 for item in list:
-    print(item)
+    print ("* " + item)
+    
 
 
