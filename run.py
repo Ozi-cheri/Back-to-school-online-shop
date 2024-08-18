4f85ec4e8b0492c0a3a66763d2802fea4259cb1c
Ozi-cheri Ihemedu <silvernasa@myyahoo.com>
2024-08-18 19:59:55 +0000

    Add list texts and modify run.py

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
-
+list = []
 print("---Back To School OnLine Shop --- ")
 name = input("Please enter your name:")
 print("Hello "+name)
 print("Welcome To Back To School Online Shop")
 print("*************************************")
-print("Would you like to continue or exist")
-input ("Please enter Yes or No")
+
+list = ['pen 0.80€,' 'pencil 0.49€', 'notebook 1.5€,' 'backpack 13.99€,' 'lunchbox 2.5€,' 'calculator 1.30€,' 'ruler 0.99' 'erasers 0.15 ,' 'folders 0.10 ,' 'glue 1€']
+for item in list:
+    print(item)
+
+
